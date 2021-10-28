f4ac4b8cdbe3d929910cb34c53bb4dc9311697fc
Jairo Becerra <jairobecerra.mt@correo.usa.edu.co>
2021-10-28 18:56:49 -0500

    Primer commit g26

---
+++ src/main/java/seman_5/gha/g26/Clase1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seman_5.gha.g26;
+
+/**
+ *
+ * @author Familia_BeLo
+ */
+public class Clase1 {
+    public int suma(int a, int b){
+        return a+b;
+    }
+    
+}
